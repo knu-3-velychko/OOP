5af0dc695fd79a3275dbf6b8f2b50895b30da7c9
Tai <justataya@gmail.com>
2019-10-13 13:08:59 +0300

    Added assets

---
+++ Lab3/app/src/main/java/com/tai/lab3/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package com.tai.lab3;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+
+public class AndroidLauncher extends AndroidApplication {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useAccelerometer = false;
+        configuration.useCompass = false;
+    }
+}
