ccd28c6237ec5a91fff0e58e07256b47d7d7dd23
Tai <justataya@gmail.com>
2019-10-20 10:50:43 +0300

    Progress commmit

---
+++ Lab3/core/src/com/tai/lab3/Checkers.java
@@ -0,0 +1,11 @@
+package com.tai.lab3;
+
+public class Checkers {
+    short [][] matrix = {
+            {0, 0, 0, 0, 1},
+            {1, 0, 0, 0, 1},
+            {1, 0, 1, 1, 1},
+            {1, 0, 0, 0, 1},
+            {1, 1, 1, 0, 1}
+    };
+}
