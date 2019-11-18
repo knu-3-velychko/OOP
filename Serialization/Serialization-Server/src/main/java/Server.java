99587fbf0052e310b7cd106bc37a190abe3a07f9
Unknown <justataya@gmail.com>
2019-11-18 22:27:07 +0200

    Progress commit

--- Serialization/Serialization-Server/src/main/java/Server.java
+++ Serialization/Serialization-Server/src/main/java/Server.java
@@ -5,8 +5,9 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Server {
-    private ObjectOutputStream out = null;
     private ObjectInputStream in = null;
+
+    private int port;
 
     private ServerSocket serverSocket;
     private Socket client;
@@ -16,6 +17,7 @@ public class Server {
     private PrintWriter storage;
 
     public Server(int port, PrintWriter storage) {
+        this.port=port;
         logger = Logger.getLogger(Server.class.getName());
         this.storage = storage;
         try {
@@ -25,42 +27,39 @@ public class Server {
         }
     }
 
-    public boolean checkConnection() {
+    public int getPort(){
+        return port;
+    }
+
+    public void checkConnection() {
         try {
             client = serverSocket.accept();
             logger.info("Connected");
-            return true;
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return false;
     }
 
     private void closeStreams() throws IOException {
-        out.close();
         in.close();
         serverSocket.close();
     }
 
-    public Object readData() {
+    public void readData() {
         try {
-            out = new ObjectOutputStream(client.getOutputStream());
             in = new ObjectInputStream(client.getInputStream());
             logger.info("Received object");
             Object object = in.readObject();
             closeStreams();
             client.close();
             saveData((Student) object);
-            return object;
         } catch (IOException | ClassNotFoundException e) {
             logger.log(Level.INFO, "Can't read data.");
         }
-        return null;
     }
 
 
     private void saveData(Student student) {
-        System.out.println(student.toString());
         storage.println(student.toString());
         logger.log(Level.INFO, "Data stored.");
         storage.close();
